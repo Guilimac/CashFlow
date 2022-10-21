fa313d69067e8c2f92212acbc084e4ce928eb21a
Guilherme de Lima Conceição <=>
2022-10-21 18:15:53 -0300

    Register entity

---
+++ CashFlow.MemoryDb/Entities/Register.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.MemoryDb.Entities
+{
+    public class Register
+    {
+        public Register()
+        {
+            this.Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public float Value { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
