cc8a32c60e54823bf93a83fc9cb4459787e4b199
Guilherme de Lima Conceição <=>
2022-10-21 18:20:30 -0300

    register db for memory

---
+++ CashFlow.MemoryDb/IRegisterDb.cs
@@ -0,0 +1,14 @@
+﻿using CashFlow.MemoryDb.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.MemoryDb
+{
+    public interface IRegisterDb
+    {
+        public IEnumerable<Register> Registers();
+    }
+}
---
+++ CashFlow.MemoryDb/RegisterDb.cs
@@ -0,0 +1,18 @@
+﻿using CashFlow.MemoryDb.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.MemoryDb
+{
+    public class RegisterDb : IRegisterDb
+    {
+        private IEnumerable<Register> _Registers;
+        public IEnumerable<Register> Registers()
+        {
+            return _Registers;
+        }
+    }
+}
